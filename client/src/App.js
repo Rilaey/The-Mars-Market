a48e7aa342fe52e19bfede14a1703913c54619b3
Riley <94302477+Rilaey@users.noreply.github.com>
2023-03-10 16:25:18 -0500

    implemented tailwind. set up routes for home, profile, and 404 page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import NoPageFound from "./pages/NoPageFound";
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NoPageFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
