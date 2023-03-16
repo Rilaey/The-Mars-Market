7d97e972ef6409e8f32b91580b40ad57804dce7a
Albert <alberto.suarez8@outlook.com>
2023-03-16 14:36:26 -0400

    Added search bar functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,29 +18,6 @@ function Header() {
         </a>
       </div>
       <div className="flex-none gap-3">
-        <div className="form-control hidden sm:flex">
-          <input
-            type="text"
-            placeholder="Search for item"
-            className="input input-bordered"
-          />
-        </div>
-        <button className="btn btn-ghost btn-circle flex sm:hidden" >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
         <select className="select select-primary max-w-xs sm:flex overflow-y-scroll">
           {loading ? (
             <option>Loading...</option>
