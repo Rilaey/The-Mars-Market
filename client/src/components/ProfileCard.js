21941712e572014e701aa75c704222f0cf38b438
Albert <alberto.suarez8@outlook.com>
2023-03-18 13:17:45 -0400

    Auth conditional rendering functionality

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -1,15 +1,16 @@
-import { useNavigate } from "react-router-dom";
-
-export default function Card(props) {
-    const navigate = useNavigate();
-
+export default function ProfileCard(props) {
     function currencyFormat(num) {
         return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
     }
 
     return (
         <div className="card w-96 glass m-[24px]">
-            <figure><img className="h-[256.67px]" src={props.image} alt="car!" /></figure>
+            <div className="flex justify-end p-2">
+                <button className="btn btn-circle btn-outline btn-error">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
+            </div>
+            <figure><img className="h-[256.67px]" src={props.image} alt="Seller Image" /></figure>
             <div className="card-body">
                 <div className="flex justify-between">
                     <h2 className="card-title">{props.title}</h2>
@@ -17,9 +18,7 @@ export default function Card(props) {
                 <p>{currencyFormat(props.price)}</p>
                 <p className="overflow-hidden text-ellipsis whitespace-nowrap">{props.description}</p>
                 <div className="card-actions justify-end">
-                    <button onClick={() => {
-                        navigate("/item/" + props.post)
-                    }} className="btn btn-primary">Buy Now!</button>
+                    <button className="btn btn-primary">Edit</button>
                 </div>
             </div>
         </div>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProfileCard from '../components/ProfileCard';
 import Card from '../components/Card';
 import { BsPersonCircle } from 'react-icons/bs';
 import { AiOutlineMail } from 'react-icons/ai'
@@ -6,6 +7,7 @@ import {HiPhone} from 'react-icons/hi';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 import { useParams } from 'react-router-dom';
+import auth from "../utils/auth";
 
 
 export default function Profile() {
@@ -23,29 +25,46 @@ export default function Profile() {
 
   return (
     <>
-      <><div className="pt-[24px] hero bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-              {user.profilePicture ?
-                <img src={user.profilePicture} className="max-w-sm rounded-lg shadow-2xl w-[345px]" />
-                :
-                <img src="https://t3.ftcdn.net/jpg/00/57/04/58/360_F_57045887_HHJml6DJVxNBMqMeDqVJ0ZQDnotp5rGD.jpg" className="max-w-sm rounded-lg shadow-2xl w-[345px]" />
-            }
-              <div>
-                <h1 className="text-5xl font-bold flex items-center">{user?.firstName + " " + user.lastName}</h1>
-                <div className="divider"></div>
-                <div className="pb-2 flex items-center"><BsPersonCircle className='mr-[8px] text-[20px]' />{user.username}</div>
-                <p className="py-2 flex items-center"><AiOutlineMail className='mr-[8px] text-[20px]' />{user.email}</p>
-                <p className="py-2 flex items-center"><HiPhone className='mr-[8px] text-[20px]' /> {user.phoneNumber}</p>
-                <button className="my-2 btn btn-primary">New Post</button>
-                <button className="my-2 mx-2 btn btn-primary">Edit Profile Info</button>
-                <button className="my-2 mx-2 btn btn-primary">Edit Profile Picture</button>
-              </div>
-            </div>
-          </div><h1 className="flex justify-center pt-[12px] text-5xl font-bold">Your Items</h1><div className='card-container justify-center items-center flex flex-wrap'>
-              {user.posts.map(product => (
-                <Card className="card" key={product._id} title={product.title} description={product.description} price={product.price} image={product.postImgs[0]} />
-              ))}
-            </div></>
+      <>{auth.getProfile().data._id === user._id ? <><div className="pt-[24px] hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          {user.profilePicture ?
+            <img src={user.profilePicture} className="max-w-sm rounded-lg shadow-2xl w-[345px]" />
+            :
+            <img src="https://t3.ftcdn.net/jpg/00/57/04/58/360_F_57045887_HHJml6DJVxNBMqMeDqVJ0ZQDnotp5rGD.jpg" className="max-w-sm rounded-lg shadow-2xl w-[345px]" />}
+          <div>
+            <h1 className="text-5xl font-bold flex items-center">{user?.firstName + " " + user.lastName}</h1>
+            <div className="divider"></div>
+            <div className="pb-2 flex items-center"><BsPersonCircle className='mr-[8px] text-[20px]' />{user.username}</div>
+            <p className="py-2 flex items-center"><AiOutlineMail className='mr-[8px] text-[20px]' />{user.email}</p>
+            <p className="py-2 flex items-center"><HiPhone className='mr-[8px] text-[20px]' /> {user.phoneNumber}</p>
+            <button className="my-2 btn btn-primary">New Post</button>
+            <button className="my-2 mx-2 btn btn-primary">Edit Profile Info</button>
+            <button className="my-2 mx-2 btn btn-primary">Edit Profile Picture</button>
+          </div>
+        </div>
+      </div><h1 className="flex justify-center pt-[12px] text-5xl font-bold">Your Items</h1><div className='card-container justify-center items-center flex flex-wrap'>
+          {user.posts.map(product => (
+            <ProfileCard className="card" key={product._id} title={product.title} description={product.description} price={product.price} image={product.postImgs[0]} />
+          ))}
+        </div></> : <><div className="pt-[24px] hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          {user.profilePicture ?
+            <img src={user.profilePicture} className="max-w-sm rounded-lg shadow-2xl w-[345px]" />
+            :
+            <img src="https://t3.ftcdn.net/jpg/00/57/04/58/360_F_57045887_HHJml6DJVxNBMqMeDqVJ0ZQDnotp5rGD.jpg" className="max-w-sm rounded-lg shadow-2xl w-[345px]" />}
+          <div>
+            <h1 className="text-5xl font-bold flex items-center">{user?.firstName + " " + user.lastName}</h1>
+            <div className="divider"></div>
+            <div className="pb-2 flex items-center"><BsPersonCircle className='mr-[8px] text-[20px]' />{user.username}</div>
+            <p className="py-2 flex items-center"><AiOutlineMail className='mr-[8px] text-[20px]' />{user.email}</p>
+            <p className="py-2 flex items-center"><HiPhone className='mr-[8px] text-[20px]' /> {user.phoneNumber}</p>
+          </div>
+        </div>
+      </div><h1 className="flex justify-center pt-[12px] text-5xl font-bold">Seller's Items</h1><div className='card-container justify-center items-center flex flex-wrap'>
+          {user.posts.map(product => (
+            <Card className="card" post={product._id} key={product._id} title={product.title} description={product.description} price={product.price} image={product.postImgs[0]} />
+          ))}
+        </div></>}</>
     </>
   )
 }
