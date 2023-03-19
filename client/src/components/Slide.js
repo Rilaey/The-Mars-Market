23e9deb85066334bdf46d0f28abfaead515cbabb
Albert <alberto.suarez8@outlook.com>
2023-03-18 21:33:10 -0400

    Fixed carousel and added media queries for smaller devices

--- client/src/components/Slide.js
+++ client/src/components/Slide.js
@@ -1,8 +1,8 @@
 export default function Slide(props) {
 
     return (
-        <div id={"slide" + props.index} className="carousel-item relative w-full h-[500px]">
-            <img src={props.postImg} className="w-full" />
+        <div id={"slide" + props.index} className="carousel-item relative w-full sm:h-[500px] h-[250px]">
+            <img src={props.postImg} className="w-full h-full" />
             {props.singleImg ? "" : <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href={"#slide" + props.previous} className="btn btn-circle">❮</a>
                 <a href={"#slide" + props.next} className="btn btn-circle">❯</a>
