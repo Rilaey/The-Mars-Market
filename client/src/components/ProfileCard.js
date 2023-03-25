192c4ae86471016359eb3f951e3e298583de7dfa
Albert <alberto.suarez8@outlook.com>
2023-03-25 16:09:53 -0400

    Added clickable imgs, removed dropdown, and fixed dark/light mode

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -29,7 +29,14 @@ export default function ProfileCard(props) {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                     </button>
                 </div>
-                <figure><img className="h-[256.67px]" src={props.image} alt="Seller Image" /></figure>
+                <figure><img 
+            className="h-[256.67px]" 
+            src={props.image} 
+            alt="Seller's Item" 
+            onClick={() => {
+                navigate("/item/" + props.post)
+            }}
+            style={{ cursor: 'pointer' }} /></figure>
                 <div className="card-body">
                     <div className="flex justify-between">
                         <h2 className="card-title">{props.title}</h2>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    theme: ['dark'],
+    theme: ['cupcake', 'dark'],
     styled: true,
     themes: true,
     base: true,
